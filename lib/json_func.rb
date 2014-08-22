3243bfd02dba52d710c3df3152f52db8779f4bf2
Hayden Faulds <fauldsh@gmail.com>
2014-08-22 20:11:47 +0100

    make fucntion_handler a module inside json_func

--- lib/json_func.rb
+++ lib/json_func.rb
@@ -39,4 +39,17 @@ class JsonFunc
       arg
     end
   end
+
+  module FunctionHandler
+    def describe(function_name, description)
+      @method_descriptions ||= {}
+      @method_descriptions[function_name.to_s] = description
+    end
+
+    def method_descriptions
+      @method_descriptions.map { |method_name, method_description|
+        method_description.merge(name: method_name)
+      }
+    end
+  end
 end
--- spec/json_func_spec.rb
+++ spec/json_func_spec.rb
@@ -82,4 +82,47 @@ describe JsonFunc do
       subject.execute('[ "second", [ "list", [ "first", "a" ], "b" ] ]')
     ).to eq(second_result)
   end
+
+  describe JsonFunc::FunctionHandler do
+    class TestHandler
+      extend JsonFunc::FunctionHandler
+
+      describe :a_function, {
+        description: 'description',
+        args: [
+          {
+            name: 'an_argument',
+            description: 'description',
+            template: 'a_template.html'
+          },
+          {
+            name: 'a_second_argument',
+            description: 'description',
+            template: 'a_template.html'
+          }
+        ]
+      }
+      def a_function(an_argument, a_second_argument)
+
+      end
+    end
+
+    describe '.method_descriptions' do
+      it 'retuns the argument descriptions' do
+        expect(TestHandler.method_descriptions).to eq([{
+          name: 'a_function',
+          description: 'description',
+          args: [ {
+            name: 'an_argument',
+            description: 'description',
+            template: 'a_template.html'
+          }, {
+            name: 'a_second_argument',
+            description: 'description',
+            template: 'a_template.html'
+          }]}
+        ])
+      end
+    end
+  end
 end
